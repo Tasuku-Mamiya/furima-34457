3ede77b0246ade62752440f70170bd2bb9f05c63
Tasuku-Mamiya <tasukusao26@gmail.com>
2021-03-05 21:30:00 +0900

    ActiveHash

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,21 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :shipping_cost_burden
+  belongs_to :shipping_area
+  belongs_to :delivery_days
+
+  with options presence: true do
+    validates :item_name
+    validates :item_description
+    validates :category_id, numericality: { other_than: 1 }
+    validates :item_status_id, numericality: { other_than: 1 }
+    validates :shipping_cost_burden_id, numericality: { other_than: 1 }
+    validates :shipping_area_id, numericality: { other_than: 1 }
+    validates :delivery_days_id, numericality: { other_than: 1 }
+    validates :price
+  end
 end
