2ac7e6f24e2640b030a6b92af53ef73bb14637d9
Tasuku-Mamiya <tasukusao26@gmail.com>
2021-03-05 19:40:50 +0900

    画像投稿機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,6 +14,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:item_name, :category_id, :price, :item_description, :shipping_area_id, :item_status_id, :shipping_cost_burden_id, :delivery_days_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :category_id, :price, :item_description, :shipping_area_id, :item_status_id, :shipping_cost_burden_id, :delivery_days_id, :image).merge(user_id: current_user.id)
   end
 end
